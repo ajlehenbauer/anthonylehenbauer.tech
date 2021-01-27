67d67c7e4bc680a6d7f7affabfb7513456701396
ajlehenbauer <46498457+ajlehenbauer@users.noreply.github.com>
2021-01-27 17:35:56 -0500

    just more things

--- home.js
+++ home.js
@@ -1,5 +1,10 @@
 var unityInstance;
 var unityInstance2;
+var letterSpace = 5;
+var letterRate = .01;
+var filter = [false,false,false];
+//unity, web, interactive
+
 
 function startGame(game){
        
@@ -20,7 +25,9 @@ function startGame(game){
 }
 
 
-$( document ).ready(function() {
+$( document ).ready(
+    
+    function() {
     
     $('#wormModal').on('show.bs.modal', function (e) {
         $('#wormModal').addClass(' animate__zoomIn');
@@ -62,8 +69,68 @@ $( document ).ready(function() {
         
        })
     
-    
+    floatText()
 });
 
+function floatText(){
 
+    //SET VARS
+    letterSpace +=letterRate;
+   if(letterSpace>10||letterSpace<5){
+       letterRate=-letterRate;
+   }
+ 
+   //UPDATE TEXT
+ 
+    $("#title").css("letter-spacing", letterSpace+"px");
+    setTimeout(function(){floatText()},10);
+
+ }
+function setFilter(){
+    if($('#unityCheck').prop('checked')){
+        filter[0]=true;
+    }
+    else{
+        filter[0]=false;
+    }
+    if($('#webCheck').prop('checked')){
+        filter[1]=true;
+    }
+    else{
+        filter[1]=false;
+    }
+    if($('#interactiveCheck').prop('checked')){
+        filter[2]=true;
+    }
+    else{
+        filter[2]=false;
+    }
+
+    organizeProjects();
+}
+function organizeProjects(){
+    var active=0;
+    $('.project-container').each(function(index){
+        
+        if($(this).attr('tags').indexOf("unity")>-1 && filter[0]){
+            $(this).removeClass("hidden");
+            active++;
+        }
+        else if($(this).attr('tags').indexOf("web")>-1 && filter[1]){
+            $(this).removeClass("hidden");
+            active++;
+        }
+        else if($(this).attr('tags').indexOf("interactive")>-1 && filter[2]){
+            $(this).removeClass("hidden");
+            active++;
+        }
+        else if(!filter[0] && !filter[1] && !filter[2]){
+            $(this).removeClass("hidden");
+            active++;
+        }
+        else{
+            $(this).addClass("hidden");
+        }
+    });
+}
   
